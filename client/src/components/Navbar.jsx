1e585d60dc9493e69b38f8cec4d1e91fa80e5dec
Faizululum <faizululum25@gmail.com>
2025-09-02 09:47:17 +0700

    feat: responsive navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets, menuLinks } from "../assets/assets";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
+  const location = useLocation();
+  const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
+
   return (
-    <div>
+    <div className={`flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 text-gray-600 border-b border-borderColor relative transition-all ${location.pathname === "/" && "bg-light"}`}>
       <Link to="/">
         <img src={assets.logo} alt="logo" className="h-8" />
       </Link>
-      <div>
+      <div
+        className={`max-sm:fixed max-sm:h-screen max-sm:w-full max-sm:top-16 max-sm:border-t border-borderColor right-0 flex flex-col sm:flex-row items-start sm:items-center gap-4 sm:gap-8 max-sm:p-4 transition-all duration-300 z-50 ${
+          location.pathname === "/" ? "bg-light" : "bg-white"
+        } ${isOpen ? "max-sm:translate-x-0" : "max-sm:translate-x-full"}`}
+      >
         {menuLinks.map((link, i) => (
           <Link key={i} to={link.path}>
             {link.name}
           </Link>
         ))}
+
+        <div className="hidden lg:flex items-center text-sm gap-2 border border-borderColor rounded-full px-3 max-w-56">
+          <input type="text" placeholder="Search" className="w-full py-1.5 bg-transparent text-gray-600 outline-none placeholder-gray-600" />
+          <img src={assets.search_icon} alt="search" />
+        </div>
+
+        <div className="flex max-sm:flex-col items-start sm:items-center gap-6">
+          <button onClick={() => navigate("/owner")} className="cursor-pointer">
+            Dashboard
+          </button>
+          <button onClick={() => setShowLogin(true)} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all text-white rounded-lg">
+            Login
+          </button>
+        </div>
       </div>
+
+      <button className="sm:hidden cursor-pointer" aria-label="menu" onClick={() => setIsOpen(!isOpen)}>
+        <img src={isOpen ? assets.close_icon : assets.menu_icon} alt="menu" />
+      </button>
     </div>
   );
 };
