1ac008a2d9b99d20331c60512d9536d5dc3de541
Faizululum <faizululum25@gmail.com>
2025-09-02 09:06:49 +0700

    first commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { assets, menuLinks } from "../assets/assets";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+      <Link to="/">
+        <img src={assets.logo} alt="logo" className="h-8" />
+      </Link>
+      <div>
+        {menuLinks.map((link, i) => (
+          <Link key={i} to={link.path}>
+            {link.name}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
