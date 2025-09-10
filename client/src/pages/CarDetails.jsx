acd289e445e860608959f4a0517f28cc1745edaa
Faizululum <faizululum25@gmail.com>
2025-09-10 08:10:23 +0700

    feat: add booking form section

--- client/src/pages/CarDetails.jsx
+++ client/src/pages/CarDetails.jsx
@@ -1,43 +1,68 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import { assets, dummyCarData } from '../assets/assets';
-import Loader from '../components/Loader';
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { assets, dummyCarData } from "../assets/assets";
+import Loader from "../components/Loader";
 
 const CarDetails = () => {
-
-  const {id} = useParams();
+  const { id } = useParams();
   const navigate = useNavigate();
   const [car, setCar] = useState(null);
+  const currency = import.meta.env.VITE_CURRENCY;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  }
 
   useEffect(() => {
-    setCar(dummyCarData.find(car => car._id === id));
-  },[id])
+    setCar(dummyCarData.find((car) => car._id === id));
+  }, [id]);
 
   return car ? (
     <div className="px-6 md:px-16 lg:px-24 xl:px-32 mt-16">
-      <button onClick={() => navigate(-1)} className="flex items-center gap-2 mb-6 text-gray-500 cursor-pointer">
-        <img src={assets.arrow_icon} alt="back" className="rotate-180 opacity-65" />
+      <button
+        onClick={() => navigate(-1)}
+        className="flex items-center gap-2 mb-6 text-gray-500 cursor-pointer"
+      >
+        <img
+          src={assets.arrow_icon}
+          alt="back"
+          className="rotate-180 opacity-65"
+        />
         Back to all cars
       </button>
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8 lg:gap-12">
         {/* Left Side - Car Image and Details */}
         <div className="lg:col-span-2">
-          <img src={car.image} alt="car image" className="w-full h-auto md:max-h-100 rounded-xl mb-6 shadow-md object-cover" />
+          <img
+            src={car.image}
+            alt="car image"
+            className="w-full h-auto md:max-h-100 rounded-xl mb-6 shadow-md object-cover"
+          />
           <div className="space-y-6">
             <div>
-              <h1 className="text-3xl font-bold">{car.brand} {car.model}</h1>
-              <p className="text-lg text-gray-500">{car.category} . {car.year}</p>
+              <h1 className="text-3xl font-bold">
+                {car.brand} {car.model}
+              </h1>
+              <p className="text-lg text-gray-500">
+                {car.category} . {car.year}
+              </p>
             </div>
             <hr className="border-borderColor my-6" />
             <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
               {[
-                { icon: assets.users_icon, text: `${car.seating_capacity} Seats` },
+                {
+                  icon: assets.users_icon,
+                  text: `${car.seating_capacity} Seats`,
+                },
                 { icon: assets.fuel_icon, text: car.fuel_type },
                 { icon: assets.car_icon, text: car.transmission },
                 { icon: assets.location_icon, text: car.location },
-              ].map(({icon, text})=> (
-                <div key={text} className="flex flex-col bg-light p-4 rounded-lg items-center text-gray-600">
+              ].map(({ icon, text }) => (
+                <div
+                  key={text}
+                  className="flex flex-col bg-light p-4 rounded-lg items-center text-gray-600"
+                >
                   <img src={icon} alt="" className="h-5 mb-2" />
                   {text}
                 </div>
@@ -52,25 +77,53 @@ const CarDetails = () => {
             <div>
               <h1 className="text-xl font-medium mb-3">Description</h1>
               <ul className="grid grid-cols-1 sm:grid-cols-2 gap-2">
-                {
-                  ["360 Camera", "Bluetooth", "Cruise Control", "Heated Seats", "Keyless Entry", "Leather Seats", "Power Windows", "Sunroof", "USB Port", "WiFi Hotspot"].map((item) => (
-                    <li key={item} className="flex items-center text-gray-500">
-                      <img src={assets.check_icon} alt="" className="h-4 mr-2" />
-                      {item}
-                    </li>
-                  ))
-                }
+                {[
+                  "360 Camera",
+                  "Bluetooth",
+                  "Cruise Control",
+                  "Heated Seats",
+                  "Keyless Entry",
+                  "Leather Seats",
+                  "Power Windows",
+                  "Sunroof",
+                  "USB Port",
+                  "WiFi Hotspot",
+                ].map((item) => (
+                  <li key={item} className="flex items-center text-gray-500">
+                    <img src={assets.check_icon} alt="" className="h-4 mr-2" />
+                    {item}
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
         </div>
         {/* Right Side - Booking Section */}
-        <form >
-
+        <form className="shadow-lg h-max sticky top-20 rounded-xl p-6 space-y-6 text-gray-500">
+          <p className="flex items-center justify-between text-2xl text-gray-800 font-semibold">
+            {currency}
+            {car.pricePerDay}
+            <span className="text-base text-gray-400 font-normal">per day</span>
+          </p>
+          <hr className="border-borderColor my-6" />
+          <div className="flex flex-col gap-2">
+            <label htmlFor="pickup-date">Pickup Date</label>
+            <input type="date" className="border border-borderColor px-3 py-2 rounded-lg" id="pickup-date" min={new Date().toISOString().split('T')[0]} required />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="return-date">Return Date</label>
+            <input type="date" className="border border-borderColor px-3 py-2 rounded-lg" id="return-date" required />
+          </div>
+          <button onSubmit={handleSubmit} className="w-full bg-primary text-white hover:bg-primary-dull transition-all py-3 font-medium rounded-xl cursor-pointer">
+            Book Now
+          </button>
+          <p className="text-sm text-center">No credit card required to reserve</p>
         </form>
       </div>
     </div>
-  ) : <Loader />
-}
+  ) : (
+    <Loader />
+  );
+};
 
-export default CarDetails
+export default CarDetails;
