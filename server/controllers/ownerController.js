ec0eeb9c2bde5e150b452038650557654f247987
Faizululum <faizululum25@gmail.com>
2025-09-22 20:47:57 +0700

    feat(api): create car api & model

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 
+// API to change role to owner
 export const changeRoleToOwner = async (req, res) => {
   try {
     const { _id } = req.user;
@@ -10,3 +11,15 @@ export const changeRoleToOwner = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+// API to list all cars
+export const addCar = async (req, res) => {
+    try {
+        const { _id } = req.user;
+        let car = JSON.parse(req.body.carData);
+        const imageFile = req.files;
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
+    }
+}
