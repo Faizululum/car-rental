22689836ed5ca78beeeb7365fbed9830782aefb9
Faizululum <faizululum25@gmail.com>
2025-09-22 18:15:31 +0700

    feat(api): create owner (role change) api

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
 import userRouter from "./routes/userRoutes.js";
+import ownerRouter from "./routes/ownerRoutes.js";
 
 // Initialize express app
 const app = express();
@@ -19,6 +20,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 app.use("/api/user", userRouter);
+app.use("/api/owner", ownerRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
