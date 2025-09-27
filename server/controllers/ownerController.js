3bdd9e7bab323ef96cff7150b030df4839ce8435
Faizululum <faizululum25@gmail.com>
2025-09-27 20:16:10 +0700

    feat(api): create update owner image api

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -140,3 +140,37 @@ export const getDashboardData = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 }
+
+// API to update user image
+export const updateUserImage = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const imageFile = req.file;
+
+    // Upload image to ImageKit
+    const fileBuffer = fs.readFileSync(imageFile.path);
+    const response = await imagekit.upload({
+      file: fileBuffer, //required
+      fileName: imageFile.originalname, //required
+      folder: "/users",
+    });
+
+    // Optimization through imagekit URL transformation
+    var optimizedImageUrl = imagekit.url({
+      path: response.filePath,
+      transformation: [
+        { width: "400" }, // Width Resizing
+        { quality: "auto" }, // Quality Optimization
+        { format: "webp" }, // Format Conversion
+      ],
+    });
+
+    const image = optimizedImageUrl;
+    await User.findByIdAndUpdate(_id, { image });
+    
+    res.json({ success: true, message: "User image updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+}
