05caa2abb0b3444630b00e51caffbc5c99aa7031
Faizululum <faizululum25@gmail.com>
2025-10-02 19:26:18 +0700

    feat(FeaturedSection, Cars): replace dummy data with context-based car data for dynamic rendering

--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -3,10 +3,12 @@ import Title from './Title'
 import { assets, dummyCarData } from '../assets/assets'
 import CarCard from './CarCard'
 import { useNavigate } from 'react-router-dom'
+import { useAppContext } from '../context/AppContext'
 
 const FeaturedSection = () => {
 
   const navigate = useNavigate();
+  const {cars} = useAppContext();
 
   return (
     <div className="flex flex-col items-center py-24 px-6 md:px-16 lg:px-24 xl:px-32">
@@ -15,7 +17,7 @@ const FeaturedSection = () => {
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-16">
         {
-          dummyCarData.slice(0,6).map((car) => (
+          cars.slice(0,6).map((car) => (
             <div key={car._id}>
               <CarCard car={car} />
             </div>
@@ -24,7 +26,7 @@ const FeaturedSection = () => {
       </div>
       <button onClick={() => {
         navigate('/cars'); scrollTo(0,0)
-      }} className="flex items-center justify-center gap-2 px-6 py-2 border border-borderColor hover:bg-gray-50 rounded-md mt18
+      }} className="flex items-center justify-center gap-2 px-6 py-2 border border-borderColor hover:bg-gray-50 rounded-md mt-18
        cursor-pointer">
         Explore All Cars <img src={assets.arrow_icon} alt="arrow" />
       </button>
