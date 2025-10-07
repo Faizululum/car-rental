b32afd512d19525481ddec55c36a702ee107d142
Faizululum <faizululum25@gmail.com>
2025-10-07 22:52:21 +0700

    fix(CarCard): correct spelling of 'Available' and 'seating_capacity' in CarCard component

--- client/src/components/CarCard.jsx
+++ client/src/components/CarCard.jsx
@@ -11,7 +11,7 @@ const CarCard = ({car}) => {
     <div onClick={() => {navigate(`/car-details/${car._id}`); scrollTo(0,0)}} className="group rounded-xl overflow-hidden shadow-lg hover:-translate-y-1 transition-all duration-500 cursor-pointer">
         <div className="relative h-48 overflow-hidden">
             <img src={car.image} alt="Car Image" className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105" />
-            {car.isAvaliable && <p className="absolute top-4 left-4 bg-primary/90 text-white text-xs px-2.5 py-1 rounded-full">Avaliable Now</p>}
+            {car.isAvailable && <p className="absolute top-4 left-4 bg-primary/90 text-white text-xs px-2.5 py-1 rounded-full">Available Now</p>}
 
             <div className="absolute bottom-4 right-4 bg-black/80 backdrop-blur-sm text-white px-3 py-2 rounded-lg">
                 <span className="font-semibold">{currency}{car.pricePerDay}</span>
@@ -30,7 +30,7 @@ const CarCard = ({car}) => {
           <div className="grid mt-4 grid-cols-2 gap-y-2 text-gray-600">
             <div className="flex text-sm items-center text-muted-foreground">
               <img src={assets.users_icon} alt="" className="h-4 mr-2" />
-              <span className="">{car.seatingCapacity} Seats</span>
+              <span className="">{car.seating_capacity} Seats</span>
             </div>
             <div className="flex text-sm items-center text-muted-foreground">
               <img src={assets.fuel_icon} alt="" className="h-4 mr-2" />
